c368aa218a9be9bc75c54a205fccd3bb4486e291
saoprb <saoprb@gmail.com>
2015-10-22 20:53:17 +0800

    Initial commit

---
+++ src/main/java/com/sao/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sao;
+
+/**
+ * Created by saopr on 10/22/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
